7e8d1e076b6ab16a99c4a8f5ce5e59833883e64c
duongthai2003 <thaiq9577@gmail.com>
2025-10-27 00:40:26 +0700

    not found

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import LoadingIcon from "~/components/LoadingIcon";
+
+function Loading() {
+  return (
+    <div className=" h-screen flex items-center flex-col justify-center">
+      <LoadingIcon />
+    </div>
+  );
+}
+
+export default Loading;
---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import NotFoundIcon from "~/components/NotFoundIcon";
+
+function NotFound() {
+  return (
+    <div className=" h-screen flex items-center flex-col justify-center">
+      <NotFoundIcon />
+    </div>
+  );
+}
+
+export default NotFound;
